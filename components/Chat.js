6709cec6d75f4a929a8dbb62f62e3baff328313a
SolidSok <solidsok@gmail.com>
2022-09-22 18:34:44 -0700

    chat ui update

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,72 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 
 export default function Chat(props) {
   let { name, color } = props.route.params;
+  let [messages, setMessages] = useState([]);
+
   // Set the screen title to the user name entered in the start screen
   useEffect(() => {
     props.navigation.setOptions({ title: name });
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: `${name} has entered the chat.`,
+        createdAt: new Date(),
+        system: true,
+      },
+    ]);
   }, []);
-
+  const onSend = (messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages)
+    );
+  };
+  const renderBubble = props => {
+    return (
+      <Bubble
+        {...props}
+        textStyle={{
+          right: {
+            color: 'black',
+          },
+        }}
+        wrapperStyle={{
+          left: {
+            backgroundColor: 'pink',
+          },
+          right: {
+            backgroundColor: 'lightblue',
+          },
+        }}
+      />
+    );
+  };
   return (
     <View style={[{ backgroundColor: color }, styles.container]}>
-      <Text style={styles.text}>Hello {name}</Text>
+      <GiftedChat
+        messages={messages}
+        renderBubble={renderBubble}
+        onSend={messages => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {/* avoid android overlap text */}
+      {Platform.OS === 'android' ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
     </View>
   );
 }
@@ -18,11 +74,5 @@ export default function Chat(props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  text: {
-    color: '#ffffff',
   },
 });
